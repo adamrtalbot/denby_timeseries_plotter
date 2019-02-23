80a21767a4fde73852deedc017a132df352d9ca4
adamrtalbot <talbot.a.r@gmail.com>
2019-02-23 10:53:57 +0000

    Added R files

---
+++ R/app.R
@@ -0,0 +1,169 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(cowplot)
+library(shiny)
+library(scales)
+library(RSQLite)
+library(plotly)
+
+########################################################
+# Define UI for application that plots gene expression #
+########################################################
+
+ui <- fluidPage(
+   # Application title
+  titlePanel("Denby Lab Timeseries Data Plot"),
+   
+   # Sidebar with input fields 
+   verticalLayout(
+      #inputPanel(
+         textAreaInput(inputId = "genes", label = "Genes:", value = "Lsat_1_v5_gn_4_1201, Bcin02g05780", resize = "both"),
+         br(),
+         selectInput('pathosystem', 
+                     "Timeseries data:", 
+                     c("Lettuce-Botrytis" = "letbot", 
+                       "Lettuce-Sclerotinia" = "letsclero", 
+                       "Arabidopsis-Botrytis" = "athalbot"), 
+                     multiple = FALSE),
+         selectInput('dataformat', 'Data format:',
+                     c('Counts' = 'counts',
+                       'Log(2) + 1' = 'log2',
+                       'VST' = 'vst'),
+                     multiple = FALSE),
+         checkboxInput("pointButton", "Points", TRUE),
+         checkboxInput("smoothButton", "Loess Regression", FALSE),
+         checkboxInput("meanButton", "Mean and SE", TRUE),
+         checkboxInput("fixyaxis", "Fix y-Axis?", FALSE),
+     # ),
+      
+      # Show a plot of the generated distribution
+     
+     plotOutput("genePlot"), # Change so that if height = rows*2, based on number of genes in plot?
+     downloadButton(outputId = 'downloadPlot', label = "Download Plot"),
+     downloadButton(outputId = 'downloadData', label = "Download CSV")
+   )
+)
+
+
+#############################################
+# Define server logic required to draw plot #
+#############################################
+
+server <- function(input, output) {
+  
+  timeseries_db <- dbConnect(drv = RSQLite::SQLite(), "timeseries-db.sqlite")
+  
+  output$genePlot <- renderPlot({
+    
+    # Make gene_list for further use:
+    gene_list <- input$genes %>%
+      gsub(pattern = "\n", replacement = " ", x = ., perl = TRUE) %>%
+      gsub(pattern = ",", replacement = " ", x = ., perl = TRUE) %>%
+      gsub(pattern = "  ", replacement = " ", x = ., perl = TRUE)
+    gene_list <- strsplit(x = gene_list, split = " ")[[1]]
+    
+    # Retrieve data from SQL database using parameterised query:
+    
+    rs <- dbSendQuery(timeseries_db, paste('SELECT * FROM', input$pathosystem, 'WHERE gene in (:genes)'))
+    dbBind(rs, param = list(genes = gene_list))
+    select.data <- dbFetch(rs)
+    
+    # Select the expression data
+    if(input$pathosystem != "athalbot"){
+      select.data <- select.data %>% mutate_("expression" = input$dataformat)
+    }
+    
+    # Sort data by input
+    
+    select.data <- select.data %>% mutate(gene = factor(gene, levels = gene_list))
+    
+    # Create plot for data:
+    
+    gene.plot <-  ggplot(data = select.data, aes(x = timepoint, y = expression, colour = treatment, group = treatment, fill = treatment)) +
+      ylab("Expression") +
+      xlab("Hours Post Infection (hpi)") +
+      scale_y_continuous(breaks = pretty_breaks(n = 8), expand = c(0.001, 1)) +
+      scale_x_continuous(limits = c(min(select.data$timepoint), max(select.data$timepoint)), breaks = seq(9, 54, by = 3)) + 
+      labs(colour = "Treatment", group = "Treatment", fill = "Treatment") +
+      facet_wrap(~gene, scales = "free") +
+      theme(strip.background = element_blank())
+
+    if(input$pointButton == TRUE){
+      gene.plot <- gene.plot + geom_point()
+    }
+    
+    if(input$smoothButton == TRUE){
+      gene.plot <- gene.plot + geom_smooth(method = "loess", mapping = aes(fill = treatment))
+    }
+    
+    if(input$meanButton == TRUE){
+      gene.plot <- gene.plot + 
+        stat_summary(fun.y = "mean", fun.ymin = function(x) mean(x) - sd(x)/sqrt(length(x)), fun.ymax = function(x) mean(x) + sd(x)/sqrt(length(x)), geom = "ribbon", alpha = 0.3) + 
+        stat_summary(fun.y = "mean", geom = "line")
+    }
+    
+    if(input$dataformat == "log2"){
+      gene.plot <- gene.plot + ylab(bquote(~Log[2]~" (Count + 1)"))
+    } else if (input$dataformat == "count") {
+      gene.plot <- gene.plot + ylab("Count")
+    } else if (input$dataformat == "vst") {
+      gene.plot <- gene.plot + ylab("VST counts")
+    }
+    
+    if(input$pathosystem == "athalbot"){
+      gene.plot <- gene.plot + 
+        scale_x_continuous(limits = c(min(select.data$timepoint), max(select.data$timepoint)), breaks = seq(0, 48, by = 2)) +
+        ylab("Relative Expression")
+    }
+    
+    if(input$fixyaxis == TRUE){
+      gene.plot <- gene.plot +
+        scale_y_continuous(limits = c(floor(min(select.data$expression)), ceiling(max(select.data$expression))))
+    }
+    
+    out.data <- out.data <- select.data %>% 
+      arrange(timepoint) %>%
+      mutate(sample = paste0(treatment, "_tp", timepoint, "_rep", replicate)) %>% 
+      select(gene, sample, expression) %>% 
+      spread(key = sample, value = expression, fill = NA)              
+            
+    output$downloadPlot <- downloadHandler(filename = "plot.png", 
+                                           content = function(file){
+                                             ggsave(file, plot = gene.plot, device = "png", width = 16)
+                                           }
+    )
+    
+    output$downloadData <- downloadHandler(
+      filename = "gene_exp.csv", 
+      content = function(file){
+        write.csv(x = out.data, file, row.names = FALSE)
+      }
+    )
+    gene.plot
+
+  }
+  )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+#####################################
+# To do:
+# - Conversion of ATG to Lsat
+# - add ability to fix y-axis:
+  # - just add on option: scale_y_continuous(limits = c(floor(min(select.data$expression)), ceiling(max(select.data$expression))))
+# - convert to using SQL database as input
+# - retrieve data using sql query
+# - Use plotly for plotting?
+  ## easy to change, just replace plotOutput with plotlyOutput and renderPlot with renderPlotly, however it screws up the axis currently.
---
+++ R/gather_timeseries_data.R
@@ -0,0 +1,141 @@
+library(tidyr)
+library(dplyr)
+library(tibble)
+library(readr)
+library(RSQLite)
+
+# Open connection to new datbase
+
+timeseries_db <- dbConnect(drv = RSQLite::SQLite(), "timeseries-db.sqlite")
+
+## Import botrytis data ##
+
+let.data <- read.csv("../7_gene_results/lettuce/deseq/lettuce_gene_count_sizefactored_wideformat.csv") %>%
+  gather(key = treatment, value = counts, -X)
+bot.data <- read.csv("../7_gene_results/bot/deseq/bot_gene_count_sizefactored_wideformat.csv")[,1:49] %>%
+  gather(key = treatment, value = counts, -X)
+
+let.vst <- read.csv("../7_gene_results/lettuce/deseq/lettuce_gene_count_sizefactored_wideformat_vst.csv") %>%
+  gather(key = treatment, value = vst, -X) %>% mutate(vst = vst-min(vst))
+bot.vst <- read.csv("../7_gene_results/bot/deseq/bot_gene_count_sizefactored_wideformat_vst.csv")[,1:49] %>%
+  gather(key = treatment, value = vst, -X) %>% mutate(vst = vst-min(vst))
+
+vst.data <- rbind(let.vst, bot.vst) %>%
+  separate(col = treatment, into = c("treatment", "timepoint", "replicate"), sep = "_") %>%
+  select(gene = X, everything()) %>%
+  mutate(timepoint = as.numeric(gsub(pattern = "tp", replacement = "", x = .$timepoint))) %>%
+  mutate(replicate = gsub(pattern = "rep", replacement = "", x = .$replicate))
+
+let.bot.data <- rbind(let.data, bot.data) %>%
+  separate(col = treatment, into = c("treatment", "timepoint", "replicate"), sep = "_") %>%
+  select(gene = X, everything()) %>%
+  mutate(timepoint = as.numeric(gsub(pattern = "tp", replacement = "", x = .$timepoint))) %>%
+  mutate(replicate = gsub(pattern = "rep", replacement = "", x = .$replicate)) %>%
+  left_join(vst.data, by = c("gene", "treatment", "timepoint", "replicate")) %>%
+  mutate(log2 = log(counts+1, base = 2)) %>%
+  mutate(pathosystem = "let-bot")
+
+remove(let.vst)
+remove(bot.vst)
+remove(let.data)
+remove(bot.data)
+remove(vst.data)
+
+dbWriteTable(timeseries_db, "letbot", let.bot.data, overwrite = TRUE)
+
+## Import sclerotinia-lettuce data ##
+
+# Lettuce VST data
+
+let.sclero.vst.data <- read.csv("../Lettuce_sclerotinia_data/lettuce_only_factored_vst.csv") %>%
+  select("gene" = "X", everything()) %>% 
+  gather(key = treatment, value = vst, -gene) %>%
+  separate(col = treatment, into = c("treatment", "timepoint", "replicate"), sep = "_") %>%
+  mutate(timepoint = as.numeric(gsub(pattern = "T", replacement = "", x = .$timepoint))) %>%
+  mutate(replicate = gsub(pattern = "Rep", replacement = "", x = .$replicate)) %>%
+  mutate(treatment = gsub(pattern = "Control", replacement = "Mock", x = .$treatment)) %>%
+  mutate(pathosystem = "let-sclero") %>%
+  mutate(vst = vst-min(vst))
+
+# Lettuce count data
+
+let.sclero.data <- read.csv(file = "timeseries_plotter/data/lettuce_sclero_only_factored_new.csv") %>%
+  select("gene" = "X", everything()) %>% 
+  gather(key = treatment, value = counts, -gene) %>%
+  separate(col = treatment, into = c("treatment", "timepoint", "replicate"), sep = "_") %>%
+  mutate(timepoint = as.numeric(gsub(pattern = "T", replacement = "", x = .$timepoint))) %>%
+  mutate(replicate = gsub(pattern = "Rep", replacement = "", x = .$replicate)) %>%
+  mutate(treatment = gsub(pattern = "Control", replacement = "Mock", x = .$treatment)) %>%
+  mutate(pathosystem = "let-sclero") %>%
+  left_join(y = let.sclero.vst.data, by = c("gene", "treatment", "timepoint", "replicate", "pathosystem"))
+
+
+# Sclerotinia
+
+sclero.data <- read.csv(file = "timeseries_plotter/data/sclerotinia_only_sizefactored.csv") %>%
+  select("gene" = "X", everything()) %>% 
+  gather(key = treatment, value = counts, -gene) %>%
+  separate(col = treatment, into = c("treatment", "timepoint", "replicate"), sep = "_") %>%
+  mutate(timepoint = as.numeric(gsub(pattern = "T", replacement = "", x = .$timepoint))) %>%
+  mutate(replicate = gsub(pattern = "Rep", replacement = "", x = .$replicate)) %>%
+  mutate(treatment = gsub(pattern = "Control", replacement = "Mock", x = .$treatment)) %>%
+  mutate(pathosystem = "let-sclero") %>%
+  mutate(vst = NA)
+
+let.sclero.data <- rbind.data.frame(let.sclero.data, sclero.data) %>% 
+  mutate(log2 = log(counts+1, base = 2))
+
+remove(sclero.data)
+remove(let.sclero.vst.data)
+
+dbWriteTable(timeseries_db, "letsclero", let.sclero.data, overwrite = TRUE)
+
+## Import Arabidopsis data ##
+
+athal.data <- read_delim("~/Documents/HAPI Lettuce Work Folder/Experiments/Experiment 17 - Botrytis Timeseries/plotter/timeseries_plotter/data/Botrytis-Combo3.txt", 
+                              "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+  select(gene = X1, everything()) %>%
+  gather(key = treatment, value = expression, -gene) %>%
+  separate(col = treatment, into = c("timepoint", "treatment", "replicate"), sep = ":") %>%
+  mutate(timepoint = as.numeric(gsub(pattern = " hours", replacement = "", x = .$timepoint))) %>%
+  mutate(pathosystem = "athal-bot") %>%
+  left_join(y = read_csv("~/Documents/HAPI Lettuce Work Folder/Experiments/Experiment 17 - Botrytis Timeseries/plotter/timeseries_plotter/data/CATMA_probes_v4.csv"), by = c("gene" = "CATMA")) %>% 
+  select(-gene) %>% 
+  select(gene = ATG, everything())
+
+dbWriteTable(timeseries_db, "athalbot", athal.data, overwrite = TRUE)
+
+# SQL queries:
+# dbGetQuery(timeseries_db, 'SELECT * FROM athalbot WHERE gene="AT5G39610"')
+# dbGetQuery(timeseries_db, 'SELECT * FROM athalbot WHERE gene in ("AT5G39610", "AT1G61110")')
+
+# test <- dbGetQuery(timeseries_db, 'SELECT * FROM athalbot WHERE gene=:x', 
+#         params = list(x = "AT5G39610"))
+
+# test <- dbGetQuery(timeseries_db, 'SELECT * FROM athalbot WHERE gene in (:x)', 
+#         params = list(x = c("AT5G39610", "AT1G61110")))
+
+# Parameterised SQL queries;
+pathosystem <- "letbot"
+rs <- dbSendQuery(timeseries_db, paste0('SELECT * FROM ', pathosystem, ' WHERE gene in (:x)'))
+dbBind(rs, param = list(x = "Lsat_1_v5_gn_4_1201"))
+test <- dbFetch(rs)
+
+# rs <- dbSendQuery(timeseries_db, paste('SELECT * FROM', 'athalbot', 'WHERE gene in (:x)'))
+# dbBind(rs, param = list(x = "AT5G39610"))
+# test <- dbFetch(rs)
+
+## Combine data tables ##
+
+#data <- rbind.data.frame(let.bot.data, let.sclero.data, let.data)
+#rm(let.bot.data)
+#rm(let.data)
+#rm(let.sclero.data)
+#rm(athal.data)
+
+## Save workspace for importing ##
+
+#dir.create("timeseries_plotter/data")
+#save(list = "data", file = "timeseries_plotter/data/timeseries.Rdata", compress = FALSE)
+
+#dbDisconnect()
